5e6a391e7cdbc2259c72208bf92cdff42c189a8d
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2020-12-06 15:34:19 +0100

    Boletin 11

---
+++ Boletin11/src/boletin11/Ejercicio1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin11;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio1 {
+    
+    
+    public void juego(){
+        
+     int numero2,numero1=Integer.parseInt(JOptionPane.showInputDialog("Jugador 1 introduce numero"));
+        do{
+      numero2=Integer.parseInt(JOptionPane.showInputDialog("Jugador 2 adivine el numero"));
+      if(numero2<numero1){
+          System.out.println("Es mas grande");
+      }else if(numero2>numero1){
+          System.out.println("Es mas pequeño");
+      }
+        }while(numero1!=numero2);
+        System.out.println("Acertaste el numero!!!! \nFelicidades");
+    }
+}
---
+++ Boletin11/src/boletin11/Ejercicio2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin11;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio2 {
+    
+   
+    public void juego(){
+        int numero,dis,aleatorio=(int)(Math.random()*50+1);
+        //System.out.println(aleatorio); //Para probar los resultados
+        do{
+            numero=Integer.parseInt(JOptionPane.showInputDialog("Acierta el numero comprendido entre 1 y 50"));
+            dis=Math.abs(aleatorio-numero);
+            if(dis>20){
+                System.out.println("Moi lonxe");
+            }else if(dis<=20&&dis>=10){
+                System.out.println("Lonxe");
+            }else if(dis<10&&dis>5){
+                System.out.println("Preto");
+            }else
+                System.out.println("Moi preto");
+        }while(aleatorio!=numero);
+        System.out.println("Acertaste el numero!!!! \nFelicidades");
+    }
+}
