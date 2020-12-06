f8c7bd8c0f1667cbd1ec08da49cc620790d91994
bmartinezparedes <bmartinezparedes@danielcastelao.org>
2020-12-06 15:31:35 +0100

    Boletin ocho al completo

--- Boletin8/src/boletin8/Boletin8.java
+++ Boletin8/src/boletin8/Boletin8.java
@@ -19,7 +19,7 @@ public class Boletin8 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        Ejercicio6 producto1=new Ejercicio6();
+        /*Ejercicio6 producto1=new Ejercicio6();
         producto1.pedirProducto();
         producto1.pedirCantidad();
         producto1.comparar();
@@ -37,7 +37,37 @@ public class Boletin8 {
         Ejercicio6 producto4=new Ejercicio6();
         producto4.pedirProducto();
         producto4.pedirCantidad();
-        producto4.comparar();
+        producto4.comparar();*/
+        
+        Ejercicio7 ej7=new Ejercicio7();
+        String resposta;
+        do{
+        resposta=JOptionPane.showInputDialog("Menu \nArea Triangulo \nArea Cadrado \nArea Circulo \nFIN (remata) \nTeclea opcion");
+
+        switch(resposta){
+            case"Area Triangulo": ej7.calcularTriangulo();
+            break;
+            case"Area Cadrado": ej7.calcularCadrado();
+            break;
+            case"Area Circulo": ej7.calcularCirculo();
+            break;
+            case"FIN": System.out.println("Fin Programa");
+            break;
+            default: System.out.println("Opcion incorrecta");
+            
+        }
+        
+        }while(resposta.equals("FIN")==false);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
--- Boletin8/src/boletin8/Ejercicio7.java
+++ Boletin8/src/boletin8/Ejercicio7.java
@@ -5,10 +5,29 @@
  */
 package boletin8;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class Ejercicio7 {
+   
+    public void calcularTriangulo(){
+        float base=Float.parseFloat(JOptionPane.showInputDialog("Introduce base"));
+        float altura=Float.parseFloat(JOptionPane.showInputDialog("Introduce altura"));
+        System.out.println("Area= "+ base*altura/2);
+    }
+    public void calcularCadrado(){
+        float lado=Float.parseFloat(JOptionPane.showInputDialog("Introduce lado"));
+        System.out.println("Area= "+ Math.pow(lado, 2));
+    }
+    public void calcularCirculo(){
+        float radio=Float.parseFloat(JOptionPane.showInputDialog("Introduce radio"));
+        System.out.println("Area= "+ Math.PI*Math.pow(radio, 2));
+    }
+    
+    
+    
     
 }
